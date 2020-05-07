1aba7c7255e88baa36987c98482810d9f492c353
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-05-07 12:58:17 -0700

    Finsihed StoryBoard

---
+++ Albums/Albums/SongTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Enzo Jimenez-Soto on 5/7/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBAction func addSong(_ sender: Any) {
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
